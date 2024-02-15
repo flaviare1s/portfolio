09ea3460254961b5065c088f7487e31a487e058c
Flávia Reis <flaviarster@gmail.com>
2024-02-14 23:49:42 -0300

    Tentando ajustar a altura do menu

--- script.js
+++ script.js
@@ -6,3 +6,20 @@ function toggleMenu() {
 }
 
 btnMobile.addEventListener('click', toggleMenu)
+
+
+window.onload = function() {
+    var alturaCard = document.querySelector('.card').offsetHeight
+    var menu = document.getElementById('#menu')
+    var nav = document.getElementById('#nav')
+
+    nav.addEventListener('click', function() {
+        if (nav.classList.contains('active')) {
+            menu.style.height = '0'
+            nav.classList.remove('active')
+        } else {
+            menu.style.height = alturaCard + 'px'
+            nav.classList.add('active')
+        }
+    })
+}
